29befca844d78dbe00fd5eb9429ed554d51dde4f
Grzegorz Kraszewski <krashan@teleinfo.pb.edu.pl>
2016-01-09 21:22:49 +0100

    - Fixed indentation in "helloworld.s"

--- ASM/HelloWorld/helloworld.s
+++ ASM/HelloWorld/helloworld.s
@@ -2,6 +2,7 @@
 ; "Hello World" using the default output (usually console)
 ; Uses dos.library/PutStr() so requires kick 2.0+
 ; 14 processor instructions :-)
+; Assembled with "vasm -Fhunkexe -o helloworld helloworld.s"
 ; Grzegorz Kraszewski, 2016
 ; Public Domain
 ; Description in Polish: http://krashan.ppa.pl/articles/amiga_asembler
@@ -17,29 +18,26 @@ PutStr			= -948
 
 ; Note: the program lacks proper startup code, so it will not run from Workbench
 
-				LEA		DosName,A1		;dos.library name string
-				MOVEQ	#36,D0			;minimum required version (36 = Kick 2.0)
-				MOVEA.L	SysBase,A6
-				JSR		OpenLibrary(A6)
+			LEA	DosName,A1		;dos.library name string
+			MOVEQ	#36,D0			;minimum required version (36 = Kick 2.0)
+			MOVEA.L	SysBase,A6
+			JSR	OpenLibrary(A6)
 
-				TST.L	D0				;zero if OpenLibrary() failed
-				BEQ.S	NoDos			;if failed, skip to exit
+			TST.L	D0			;zero if OpenLibrary() failed
+			BEQ.S	NoDos			;if failed, skip to exit
 
-				MOVE.L	#Hello,D1			;string to print
-				MOVEA.L	D0,A6			;moving DOSBase to A6
-				JSR		PutStr(A6)
+			MOVE.L	#Hello,D1		;string to print
+			MOVEA.L	D0,A6			;moving DOSBase to A6
+			JSR	PutStr(A6)
 
-				MOVEA.L	A6,A1			;DOSBase, library to close
-				MOVEA.L	SysBase,A6
-				JSR		CloseLibrary(A6)
+			MOVEA.L	A6,A1			;DOSBase, library to close
+			MOVEA.L	SysBase,A6
+			JSR	CloseLibrary(A6)
 
-NoDos			CLR.L	D0				;return 0 to the system
-				RTS
+NoDos			CLR.L	D0			;return 0 to the system
+			RTS
 
 ; Data
 
 DosName			DC.B		"dos.library",0
 Hello			DC.B		"Hello World!",10,0
-
-
-
