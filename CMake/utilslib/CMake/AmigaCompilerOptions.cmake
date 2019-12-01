18f1e30ec2d0bff343c8b198b95f5cd6a37a2c5f
skeetor <sparhawk@gmx.at>
2019-12-01 12:13:59 +0100

    Added ASM options to global build so that CMakeLists.txt becomes simpler for each project

--- CMake/utilslib/CMake/AmigaCompilerOptions.cmake
+++ CMake/utilslib/CMake/AmigaCompilerOptions.cmake
@@ -6,14 +6,15 @@ cmake_minimum_required(VERSION 3.14)
 # when linking a library:
 # <library>.a: error adding symbols: archive has no index; run ranlib to add one
 get_filename_component(COMPILER_DIRECTORY "${CMAKE_C_COMPILER}" DIRECTORY)
-set(CMAKE_AR "${COMPILER_DIRECTORY}/amiga-ar")
-set(CMAKE_RANLIB "${COMPILER_DIRECTORY}/amiga-ranlib") 
+set(CMAKE_AR "${COMPILER_DIRECTORY}amiga-ar")
+set(CMAKE_RANLIB "${COMPILER_DIRECTORY}amiga-ranlib") 
 
+set(UTILSLIB_OPTIONS "gcc")
 set(UTILSLIB_BUILD_TYPE "")
 
-if (CMAKE_BUILD_TYPE STREQUAL  "")
+if ("${CMAKE_BUILD_TYPE}" STREQUAL  "")
     set(CMAKE_BUILD_TYPE "Release")
-endif(CMAKE_BUILD_TYPE STREQUAL  "") 
+endif("${CMAKE_BUILD_TYPE}" STREQUAL  "") 
 
 # Set compiler flags which should be kept for both C and C++.
 SET(COMMON_COMPILER_FLAGS "--save-temps -Wall -pedantic -mcrt=nix13")
@@ -22,17 +23,19 @@ SET(COMMON_COMPILER_FLAGS "--save-temps -Wall -pedantic -mcrt=nix13")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${COMMON_COMPILER_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${COMMON_COMPILER_FLAGS}")
 
-if (CMAKE_BUILD_TYPE STREQUAL  "Debug")
+if ("${CMAKE_BUILD_TYPE}" STREQUAL  "Debug")
     message("-- Building debug version")
     set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -O0 -g -mregparm")
-endif(CMAKE_BUILD_TYPE STREQUAL  "Debug") 
+endif("${CMAKE_BUILD_TYPE}" STREQUAL  "Debug") 
 
-if(CMAKE_BUILD_TYPE STREQUAL  "Release") 
+if("${CMAKE_BUILD_TYPE}" STREQUAL  "Release") 
     message("-- Building release version")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O3 -mregparm")
-endif(CMAKE_BUILD_TYPE STREQUAL  "Release")  
+endif("${CMAKE_BUILD_TYPE}" STREQUAL  "Release")  
 
 set(UTILSLIB_BUILD_TYPE "${CMAKE_BUILD_TYPE}")
+
+include(AmigaASMOptions)
 
 include_directories(${UTILSLIB_PATH})
 include_directories(${UTILSLIB_DIR})
